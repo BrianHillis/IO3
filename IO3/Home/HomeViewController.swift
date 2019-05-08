b8c85c17bd44d12d75e589d21d05a205e73a5cac
Christian Caldwell <cgcnbf@mail.missouri.edu>
2019-05-08 18:35:34 -0500

    Some CSS with a splash of razzle dazzle

--- IO3/Home/HomeViewController.swift
+++ IO3/Home/HomeViewController.swift
@@ -54,7 +54,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 //    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
