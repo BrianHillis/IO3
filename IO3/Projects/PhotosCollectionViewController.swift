2e8fb5e1c1c550d78374e6852337608d7aeaa75c
Jason <jasontesreau@gmail.com>
2019-05-14 16:51:27 -0500

    broken photo core data stuff

--- IO3/Projects/PhotosCollectionViewController.swift
+++ IO3/Projects/PhotosCollectionViewController.swift
@@ -13,13 +13,15 @@ private let reuseIdentifier = "photoCell"
 
 class PhotosCollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var addPhotos: UIBarButtonItem!
+    
     @IBOutlet var myCollectionView: UICollectionView!
     
+    @IBOutlet weak var imageView: UIImageView!
     
     
     
-    
+    var selectedImageTag = 0
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var imagesArray = [UIImage]()
 //    var newImage: UIImage?
 //
@@ -31,7 +33,7 @@ class PhotosCollectionViewController: UICollectionViewController, UIImagePickerC
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+       fetchData()
 //        imagesArray = imageDefaults.array(forKey: "imagesArray") as! [UIImage]
 
         // Register cell classes
@@ -57,9 +59,84 @@ class PhotosCollectionViewController: UICollectionViewController, UIImagePickerC
         self.present(picker, animated: true, completion: nil)
     }
     
+    func fetchData() {
+        let container = appDelegate.persistentContainer
+        let context = container.viewContext
+        let fetchRequest = NSFetchRequest<Image>(entityName: "Image")
+        
+        do{
+            let images = try context.fetch(fetchRequest)
+            
+            for image in images {
+                if let placement = image.placement,
+                    let filePath = image.filePath {
+                    
+                    if FileManager.default.fileExists(atPath: filePath) {
+                        if let contentsOfFilePath = UIImage(contentsOfFile: filePath) {
+                            switch placement {
+                            case "top": imageView.image = contentsOfFilePath
+                            default: break
+                            }
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("in catch for image fetch request")
+        }
+    }
+    
     
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        let fileManager = FileManager.default
+        
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        print("docURL: \(documentsURL)")
+        
+        let documentPath = documentsURL.path
+        print("docPath: \(documentPath)")
+        
+        let filePath = documentsURL.appendingPathComponent("\(String(selectedImageTag)).png")
+        print("filePath: \(filePath)")
+        
+        
+        do{
+            let files = try fileManager.contentsOfDirectory(atPath: "\(documentPath)")
+            
+            for file in files {
+                if "\(documentPath)/\(file)" == filePath.path {
+                    try fileManager.removeItem(atPath: filePath.path)
+                }
+            }
+        } catch {
+            print("Couldnt add image from doc directory: \(error)")
+        }
+        
+        do {
+            if let pngImageData = UIImagePNGRepresentation(image) {
+                try pngImageData.write(to: filePath, options: .atomic)
+            }
+        } catch {
+            print("couldnt write image")
+        }
+        
+        let container  = appDelegate.persistentContainer
+        let context = container.viewContext
+        let entity = Image(context: context)
+        entity.filePath = filePath.path
+        
+        switch selectedImageTag {
+        case 1: entity.placement = "top"
+        case 2: entity.placement = "middle"
+        case 3: entity.placement = "bottom"
+        default:
+            break
+        }
+        appDelegate.saveContext()
+        
         
         if let pickedimage = (info[.originalImage] as? UIImage){
             imagesArray = [pickedimage]
